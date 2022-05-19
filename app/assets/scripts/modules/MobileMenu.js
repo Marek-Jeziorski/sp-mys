cdaa5efc6a20772cf6f2dcb9d4abf19759f71cee
castancarl <castancarl@gmail.com>
2022-05-19 21:27:05 +0200

    MAIN-NAV - mobile menu
    opacity instead display:block, transition, transform,

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,16 +1,18 @@
 class MobileMenu {
   constructor() {
-    this.menuIcon = document.querySelector('.header__nav-icon');
-    this.menuContent = document.querySelector('.header__navbar');
+    this.navIcon = document.querySelector('.header__nav-icon');
+    this.navBar = document.querySelector('.header__navbar');
+    this.header = document.querySelector('header');
     this.events();
   }
 
   events() {
-    this.menuIcon.addEventListener('click', () => this.toggleTheMenu());
+    this.navIcon.addEventListener('click', () => this.toggleTheMenu());
   }
 
   toggleTheMenu() {
-    this.menuContent.classList.toggle('header__navbar--is-visible');
+    this.navBar.classList.toggle('header__navbar--is-visible');
+    this.header.classList.toggle('header--is-expanded');
   }
 }
 
