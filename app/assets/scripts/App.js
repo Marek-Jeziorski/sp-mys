d8842e1076959e56dd029c48254d7100bdeb2687
castancarl <castancarl@gmail.com>
2022-05-25 17:35:10 +0200

    end revealing elements

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,8 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 let mobileMenu = new MobileMenu();
-let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll('.programs__img'), 75);
+new RevealOnScroll(document.querySelectorAll('.footer__col'), 66);
 
 /* ---------------------------------------------------------------------------- */
 if (module.hot) {
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,23 +1,55 @@
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
+
 class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = document.querySelectorAll('.programs__img');
+  constructor(elements, tresholdPercent) {
+    this.itemsToReveal = elements;
+    this.tresholdPercent = tresholdPercent;
+    this.browserHeight = window.innerHeight;
     this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener('scroll', () => {
-      console.log('Scroll function run');
-      this.itemsToReveal.forEach((el) => this.calculateIfScrolledTo(el));
-    });
+    window.addEventListener('scroll', this.scrollThrottle);
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        console.log('Resize just run');
+        this.browserHeight = window.innerHeight;
+      }, 333)
+    );
   }
 
-  hideInitially() {
-    this.itemsToReveal.forEach((el) => el.classList.add('reveal-item'));
+  calcCaller() {
+    console.log('Scroll function run');
+    this.itemsToReveal.forEach((el) => this.calculateIfScrolledTo(el));
   }
 
   calculateIfScrolledTo(el) {
-    console.log(el.getBoundingClientRect().y);
+    if (window.scrollY + window.innerHeight > el.offsetTop) {
+      console.log('Element was calculated');
+      let scrollPercent =
+        (el.getBoundingClientRect().y / window.innerHeight) * 100;
+
+      if (scrollPercent < this.tresholdPercent) {
+        console.log(this.tresholdPercent);
+        el.classList.add('reveal-item--is-visible');
+        el.isRevealed = true;
+        if (el.isLastItem) {
+          window.removeEventListener('scroll', this.scrollThrottle);
+        }
+      }
+    }
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach((el) => {
+      el.classList.add('reveal-item');
+      el.isRevealed = false;
+    });
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 
