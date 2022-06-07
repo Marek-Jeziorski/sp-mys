3acb026fc4c29bb1ef51ce508a7b1f44769525fc
castancarl <castancarl@gmail.com>
2022-06-07 16:03:53 +0200

    MODAL-OVERLAY -- END
    Dodałem także use calss _section-title.css oraz _social-icons.css
    DO NADCHODZĄCEGO TEMATU O FONTACH I ICONACH

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,12 +4,29 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let stickyHeader = new StickyHeader();
-
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.programs__img'), 75);
 new RevealOnScroll(document.querySelectorAll('.footer__col'), 66);
+new MobileMenu();
 
-let mobileMenu = new MobileMenu();
+/* ---MODAL--- */
+let modal;
+document.querySelectorAll('.open-modal').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (typeof modal == 'undefined') {
+      import(/* webpackChunkName: "overlay - modal" */ './modules/Modal')
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log('There was a problem.'));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
+
 /* ---------------------------------------------------------------------------- */
 if (module.hot) {
   module.hot.accept();
