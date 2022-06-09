457c151bbb7e97538a7809533d31408c985b2b59
castancarl <castancarl@gmail.com>
2022-06-09 16:04:15 +0200

    BUILDING-WORK - separate dev/build
    Wepack ma teraz tryb produkcji tj:
    
    Do folderu DIST/DOCS -->
    
    a) tworzy kopię paczki głównej bundled.js, która zawiera wszystkie .css i .js podpięte do ENTRY POINT APP.JSS
    b) tworzy paczkę 000.bundled.js , która zawiera    mój MODAL (overlay)

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,51 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 
-const postCSSPlugins = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer'),
-];
+const postCSSPlugins = [require('postcss-import'), require('postcss-mixins'), require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer')];
 
-module.exports = {
+/* ---------------------------- COMMON FOR DEV/BUILD ----------------------- */
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } }],
+      },
+    ],
+  },
+};
+
+/* ---------------------------- DEV TASK ---------------------------------- */
+if (currentTask == 'dev') {
+  config.mode = 'development';
+
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app'),
-  },
-  devServer: {
+  };
+
+  // WEBPACK SERVER
+  config.devServer = {
     before: function (app, server) {
       server._watch('./app/**/*.html');
     },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
-    port: 3001,
+    port: 3000,
     host: '0.0.0.0',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader?url=false',
-          {
-            loader: 'postcss-loader',
-            options: { postcssOptions: { plugins: postCSSPlugins } },
-          },
-        ],
-      },
-    ],
-  },
-};
+  };
+}
+
+/* ---------------------------- BUILD TASK ---------------------------------- */
+if (currentTask == 'build') {
+  config.mode = 'production';
+
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'docs'),
+  };
+}
+
+module.exports = config;
