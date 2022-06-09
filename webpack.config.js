dcb0993fe7b80faff923b9e8d0a46fa43ad500c3
castancarl <castancarl@gmail.com>
2022-06-09 18:47:49 +0200

    BUILDING-WORK - chunkhash and clear
    new bundle with chunkhash name
    then clear all ones

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const postCSSPlugins = [require('postcss-import'), require('postcss-mixins'), require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer')];
 
@@ -51,6 +52,8 @@ if (currentTask == 'build') {
   config.optimization = {
     splitChunks: { chunks: 'all' },
   };
+
+  config.plugins = [new CleanWebpackPlugin()];
 }
 
 module.exports = config;
