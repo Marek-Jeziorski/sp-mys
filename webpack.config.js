5912ff7d2f8c2bf38fc78fb210a8a7839ee35505
castancarl <castancarl@gmail.com>
2022-06-11 14:42:17 +0200

    BUIlD-WORK -- extract/min CSS
    extract from bundled.js CSS-Styles and optimize minified them .

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const postCSSPlugins = [require('postcss-import'), require('postcss-mixins'), require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer')];
 
@@ -20,14 +21,8 @@ let cssConfig = {
 
 let config = {
   entry: './app/assets/scripts/App.js',
-
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } }],
-      },
-    ],
+    rules: [cssConfig],
   },
 };
 
@@ -36,7 +31,7 @@ if (currentTask == 'dev') {
   config.mode = 'development';
 
   // [cssConfig.use] is an array so we can say dot unshift to add an item to the beginning of the array.
-  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  cssConfig.use.unshift('style-loader');
 
   config.output = {
     filename: 'bundled.js',
@@ -50,7 +45,7 @@ if (currentTask == 'dev') {
     },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
-    port: 3000,
+    port: 3001,
     host: '0.0.0.0',
   };
 }
@@ -69,9 +64,11 @@ if (currentTask == 'build') {
   };
 
   config.optimization = {
-    splitChunks: { chunks: 'all' },
+    splitChunks: { chunks: 'all', minSize: 1000 },
+    minimize: true,
+    minimizer: ['...', new CssMinimizerPlugin()],
   };
-  config.plugins = [new CleanWebpackPlugin(), new MiniCssExractPlugin()];
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' })];
 }
 
 module.exports = config;
