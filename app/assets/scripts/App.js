79c04e612b037c9e5d2a5f6637559e751f62f126
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-12-04 22:47:16 +0100

    up

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,27 +1,27 @@
-import '../styles/styles.css';
-import 'lazysizes';
+import "../styles/styles.css";
+import "lazysizes";
 
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 new StickyHeader();
-new RevealOnScroll(document.querySelectorAll('.programs__img'), 75);
+new RevealOnScroll(document.querySelectorAll(".programs__img"), 75);
 /* new RevealOnScroll(document.querySelectorAll('.footer__col'), 66); */
 new MobileMenu();
 
 /* ---MODAL--- */
 let modal;
-document.querySelectorAll('.open-modal').forEach((el) => {
-  el.addEventListener('click', (e) => {
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
     e.preventDefault();
-    if (typeof modal == 'undefined') {
-      import(/* webpackChunkName: "overlay - modal" */ './modules/Modal')
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "overlay - modal" */ "./modules/Modal")
         .then((x) => {
           modal = new x.default();
           setTimeout(() => modal.openTheModal(), 20);
         })
-        .catch(() => console.log('There was a problem.'));
+        .catch(() => console.log("There was a problem."));
     } else {
       modal.openTheModal();
     }
